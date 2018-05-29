24b16f0ef262ac32b3cf0f3309efc2bd1b71ec96
ognjenpe <ognjenpejovic@gmail.com>
2018-05-29 23:03:41 +0200

    Dodati layouti

--- app/src/main/java/rs/aleph/android/example12/activities/provider/SecondActivity.java
+++ app/src/main/java/rs/aleph/android/example12/activities/provider/SecondActivity.java
@@ -31,7 +31,7 @@ public class SecondActivity extends Activity {
         // Each lifecycle method should call the method it overrides
         super.onCreate(savedInstanceState);
         // setContentView method draws UI
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_second_relative);
 
         // Shows a toast message (a pop-up message)
         Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onCreate()", Toast.LENGTH_SHORT);
